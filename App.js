9639fdf02176e2fb2ef2ef8a2dc0dc6162492a0b
ODakatA <dstoilov199@gmail.com>
2022-03-11 16:20:24 +0200

    Resolving Merge Conflicts

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import LeaderBoard from "./screens/Leaders";
 
 const App = () => {
   return <LeaderBoard />;
