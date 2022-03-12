51f693fc71a67402637ac4905d6dc8da329dfdb7
ODakatA <dstoilov199@gmail.com>
2022-03-12 09:05:29 +0200

    Login bugs fixed

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
-import {onAuthStateChanged} from 'firebase/firestore'
 
 const Stack = createNativeStackNavigator();
 
@@ -16,14 +15,20 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName={user ? "Home" : "Login"}
         screenOptions={{
           headerShown: false,
         }}
-        component={user ? HomeScreen : LoginScreen}
-        // taq proverka tuk mai ne bachka, poneje vliza v home direktno
+        component={HomeScreen}
       >
-        <Stack.Screen name="Home" component={HomeScreen}/>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            user,
+          }}
+        />
 
         <Stack.Screen name="Login" component={LoginScreen} />
 
