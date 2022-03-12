c6d59c3e97290fd82bdc4a8c0739c263e9f957e1
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2022-03-12 12:39:25 +0200

    DailyFact without image

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import DailyFact from "./screens/DailyFact";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ const App = () => {
   const user = auth.currentUser;
 
   return (
+    // <DailyFact/>
+    
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName={user ? "Home" : "Login"}
